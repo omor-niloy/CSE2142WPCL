57488c324457b4b9722d9a9e54bd1dd4faa19da0
omor-niloy <omor.niloy.0@gmail.com>
2022-08-17 10:50:35 +0600

    updated constant class

--- Constant.java
+++ Constant.java
@@ -10,9 +10,9 @@ public class Constant{
     public String Invalid = "Invalid input";
     public String FileNmae = "students.txt";
     public String split = ", ";
-    public String showNames = "a";
-    public String randomNmae = "r";
-    public String addName = "+";
-    public String query = "?";
-    public String countWords = "c";
+    public String showNames = "a"; // command line argument to display all student's name
+    public String randomNmae = "r";// command line argument to display random name
+    public String addName = "+"; // command line argument to add name
+    public String query = "?"; // command line argument to search a student name
+    public String countWords = "c"; // command line argument to count the student names
 }
